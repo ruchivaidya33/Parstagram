ecf3810799ea9729f1ad244f7e578e3e4f19967c
Honors <honors@honorslapt-b59853.local>
2019-03-20 13:44:51 -0400

    Parstagram Part 1

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Honors on 3/20/19.
+//  Copyright © 2019 Ruchi Vaidya. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
