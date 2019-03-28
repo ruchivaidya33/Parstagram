b6b215169338595c7dbfacea1bb0aa93bde910f6
Honors <honors@honorslapt-b59853.local>
2019-03-28 14:13:29 -0400

    Part 2 of Parstagram App!

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -24,6 +24,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://immense-everglades-94051.herokuapp.com/parse"
             })
         )
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            
+            window?.rootViewController = feedNavigationController
+        }
+        
+        
         return true
     }
 
---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Honors on 3/28/19.
+//  Copyright © 2019 Ruchi Vaidya. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
